3d923985e0cef1d0be526298a367bf0460ca3d24
SigmaMiao <sigmamiao@yahoo.com>
2015-09-28 22:07:22 +0800

    添加用户权限Role

--- app/models.py
+++ app/models.py
@@ -120,9 +120,45 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW |
+                     Permission.COMMENT |
+                     Permission.WRITE_STORY, True),
+            'Moderator': (Permission.FOLLOW |
+                          Permission.COMMENT |
+                          Permission.WRITE_STORY |
+                          Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]  # add (Permission, )
+            role.default = roles[r][1]  # add (, True or False)
+            db.session.add(role)
+        db.session.commit()
+
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+
+class Permission:
+    def __init__(self):
+        pass
+
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_STORY = 0x004
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 
 @loginManager.user_loader
